538cf4ebc5cf90e68ac50fdb3377be259d9901f8
razzlepdx <rwilson.or@gmail.com>
2017-08-24 06:27:00 -0700

    add ideas submitted/rendered through db

--- app.js
+++ app.js
@@ -25,38 +25,38 @@ app.set('view engine', 'ejs');
 // Test data (until we set up the db)
 // -----------------------------
 
-var ideas = [
-  {
-    title: "Web app to collect project ideas",
-    blurb: "People can share project ideas, for discussion, ranking, and build",
-    author: "Reuben",
-    dateSubmitted: "23 minutes ago",
-  },
-  {
-    title: "Goal tracker for FCC",
-    blurb: "I want to be able to see where I'm at in the FCC curriculum, at a glance",
-    author: "Joe",
-    dateSubmitted: "7 hours ago",
-  },
-  {
-    title: "Healthy meal choices!",
-    blurb: "A tinder-style app that gives recipe suggestions and meal plans, based on preferences and diet",
-    author: "Reuben",
-    dateSubmitted: "Yesterday",
-  },
-  {
-    title: "Netflix for independent movie makers",
-    blurb: "It's just like Netflix, but indie movie producers can upload their own films",
-    author: "Gandalf the Grey",
-    dateSubmitted: "15/5/17",
-  },
-  {
-    title: "Hallow Puppy!",
-    blurb: "An app to quickly visualize different halloween costumes on puppies. A little bit like the snapchat",
-    author: "Rusty",
-    dateSubmitted: "1/12/16",
-  },
-]
+// var ideas = [
+//   {
+//     title: "Web app to collect project ideas",
+//     blurb: "People can share project ideas, for discussion, ranking, and build",
+//     author: "Reuben",
+//     dateSubmitted: "23 minutes ago",
+//   },
+//   {
+//     title: "Goal tracker for FCC",
+//     blurb: "I want to be able to see where I'm at in the FCC curriculum, at a glance",
+//     author: "Joe",
+//     dateSubmitted: "7 hours ago",
+//   },
+//   {
+//     title: "Healthy meal choices!",
+//     blurb: "A tinder-style app that gives recipe suggestions and meal plans, based on preferences and diet",
+//     author: "Reuben",
+//     dateSubmitted: "Yesterday",
+//   },
+//   {
+//     title: "Netflix for independent movie makers",
+//     blurb: "It's just like Netflix, but indie movie producers can upload their own films",
+//     author: "Gandalf the Grey",
+//     dateSubmitted: "15/5/17",
+//   },
+//   {
+//     title: "Hallow Puppy!",
+//     blurb: "An app to quickly visualize different halloween costumes on puppies. A little bit like the snapchat",
+//     author: "Rusty",
+//     dateSubmitted: "1/12/16",
+//   },
+// ]
 
 
 //test db to see if Mongoose is working with Mongo
@@ -64,23 +64,25 @@ var ideas = [
 var ideaSchema = new mongoose.Schema({
 	title: String,
   description: String,
+  author: String,
+  dateSubmitted: String
 });
 
 var Idea = mongoose.model("Idea", ideaSchema);
 
-Idea.create(
-	{
-			title: "Meditation yoga project",
-			description: "An app where you can find people who like both meditation and yoga."
-	}, function (err, idea){
-		if(err){
-			console.log(err);
-		} else {
-			console.log("YOU DID IT! Idea created! :D");
-			console.log(idea);
-		}
-	}
-);
+// Idea.create(
+// 	{
+// 			title: "Meditation yoga project",
+// 			description: "An app where you can find people who like both meditation and yoga."
+// 	}, function (err, idea){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("YOU DID IT! Idea created! :D");
+// 			console.log(idea);
+// 		}
+// 	}
+// );
 
 // ----------------------------------------------------------
 // Routes
@@ -88,7 +90,13 @@ Idea.create(
 
 // Homepage (Ideas)
 app.get('/', function(req, res) {
-  res.render('home', {ideas: ideas});
+  Idea.find({}, function (err, allIdeas) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', {ideas: allIdeas});
+    }
+  }); 
 });
 
 // Projects
@@ -97,13 +105,20 @@ app.get('/projects', function(req, res) {
 });
 
 app.post('/ideas', function(req, res) {
-  // Get data from form and add to ideas array
-  var title = req.body.ideaTitle || 'Test title';
-  var description = req.body.ideaDescription || 'Test description';
-  var newIdea = { title: title, blurb: description, author: 'Anonymous', dateSubmitted: 'Just Now'};
-  ideas.unshift(newIdea);
-  // Redirect to ideas page
-  res.redirect('/');
+  // Get data from form and add to db
+  var title = req.body.ideaTitle;
+  var description = req.body.ideaDescription;
+  var newIdea = { title: title, description: description, author: 'Anonymous', dateSubmitted: 'Just Now'};
+  Idea.create(newIdea, function (err, newlyCreated) {
+    //error check
+    if (err) {
+      // TODO: Add message to user about error
+      console.log(err);
+    } else {
+       // Redirect to ideas page
+      res.redirect('/');
+    }
+  });
 });
 
 // Sign up
