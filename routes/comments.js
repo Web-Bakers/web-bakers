660abfc0f5df533c055b0db30dcb712742081d02
Joseph Previte <josephprevite@Josephs-MacBook-Pro.local>
2017-11-28 07:56:43 -0700

    Add required packages and files

---
+++ routes/comments.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var User = require('../models/user');
+var Idea = require('../models/idea');
+var Comment = require('../models/comment');
+var router = express.Router();
+
+//add comment routes here.
+
+module.exports = router;
